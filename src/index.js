9a43f5d7fcc0ce026a0197f28d8fc451eec5da7c
Tay0108 <kjkosecki@gmail.com>
2020-03-16 14:48:11 +0100

    fetching data for multiple shops

--- src/index.js
+++ src/index.js
@@ -5,9 +5,22 @@ const app = express();
 const port = 3001;
 
 app.get("/", async (req, res) => {
+    const shopIds = [
+        "ChIJY9sg1U_xPUcRMJUmEHnfnDg", // Żabka
+        "ChIJcVatSK_xPUcRnV0LvEKspkE", // Delikatesy Centrum
+        "ChIJ9YyJTzfxPUcRuhHc6ieWB8M", // Carrefour Express
+        "ChIJ7cVX8DnxPUcR8-dpVwsYgx8", // Bonus 2
+    ];
+
     try {
-        const data = await got(`https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJY9sg1U_xPUcRMJUmEHnfnDg&fields=name,rating,opening_hours&key=${process.env.API_KEY}`);
-            res.send(data.body);
+        const shopData = [];
+
+        for(const shopId of shopIds) {
+            const data = await got(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${shopId}&fields=name,rating,opening_hours&key=${process.env.API_KEY}`);
+            shopData.push(JSON.parse(data.body));
+        }
+            console.log(shopData);
+            res.send(shopData);
     } catch(error) {
         console.log(error);
         res.sendStatus(500);
